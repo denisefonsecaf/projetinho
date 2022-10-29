ee30e368b711ab9af5121ae442e104556199880f
Denise <denise.fonseca.flausino@gmail.com>
2022-10-29 10:45:51 -0300

    segundo commit

---
+++ efeito.js
@@ -0,0 +1,21 @@
+const el = document.querySelector('#text');
+const text = "Você sabia?";
+const interval = 150;
+
+function showText(el, text, interval){
+    const char = text.split("").reverse();
+
+    const typer = setInterval( () => {
+
+        if(!char.length){
+            return clearInterval(typer);
+        }
+
+        const next = char.pop();
+        
+        el.innerHTML += next;
+
+    }, interval);
+}
+
+showText(el, text, interval);
